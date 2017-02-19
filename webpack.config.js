26ca8287671d5d14f2bb37bcc122579777e4f89e
Beto Muniz <contato@betomuniz.com>
2017-02-19 19:14:52 -0300

    Starting the refactoring

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const DEV_MODE = process.env.NODE_ENV === 'development';
+
+module.exports = {
+    entry: {
+      main: path.resolve(__dirname, 'src')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.html/,
+            use: [{
+              loader: 'html-loader'
+            }]
+          },
+          {
+            test: /\.scss$/,
+            use: DEV_MODE ? [{
+              loader: 'style-loader'
+            }, {
+              loader: 'css-loader'
+            }, {
+              loader: 'sass-loader'
+            }] : ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader', 'sass-loader'] })
+        },
+        {
+          test: /\.js/,
+          use: [{
+            loader: 'babel-loader',
+            options: {
+              ignore: "node_modules",
+              comments: false
+            }
+          }]
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: 'images/[sha512:hash:base64:7].[ext]'
+            }
+          }]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html'
+      }),
+      new CopyWebpackPlugin([
+        { from: './src/images/misc', to: 'images/misc' }
+      ]),
+      new ExtractTextPlugin("styles.css"),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': DEV_MODE ? '"development"' : '"production"',
+      })
+    ],
+};
