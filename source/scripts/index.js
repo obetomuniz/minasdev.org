bf7bdd397f43e44f37bfcfc5f06862d954a18271
Beto Muniz <contato@betomuniz.com>
2017-12-18 02:43:39 -0200

    Fix cache of request in fetch

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -36,7 +36,14 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Fetching the events
-  fetch('https://api.minasdev.org/events')
+  let myHeaders = new Headers();
+  myHeaders.append('pragma', 'no-cache');
+  myHeaders.append('cache-control', 'no-cache');
+
+  fetch('https://api.minasdev.org/events', {
+    method: 'GET',
+    headers: myHeaders
+  })
     .then(response => {
       // If online: Render and save locally the event list
       response.json().then(events => {
