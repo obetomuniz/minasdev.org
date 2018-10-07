37bff7ad4ae012cd434fd16c82330275175e81c6
Beto Muniz <contato@betomuniz.com>
2018-10-07 01:23:10 -0300

    Remove trash code

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,8 +1,4 @@
 import { takeEvery, all } from "redux-saga/effects";
-import {
-  FETCH_MINASDEV_EVENTS_REQUEST,
-  FETCH_MINASDEV_JOBS_REQUEST
-} from "@reducers/MinasDev/types";
 
 export default function* rootSaga() {
   yield all([]);
