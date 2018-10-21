dc89bd431c014048c151aa8e3b8c2e5141267be7
Beto Muniz <contato@betomuniz.com>
2018-10-21 12:22:09 -0300

    Close #26

--- src/components/Common/ScreenReaderContent/index.js
+++ src/components/Common/ScreenReaderContent/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Wrapper } from "./UI";
 
-const ScreenReaderContent = ({ children }) => <Wrapper>{children}</Wrapper>;
+const ScreenReaderContent = props => <Wrapper {...props}>{props.children}</Wrapper>;
 
 ScreenReaderContent.propTypes = {
   children: PropTypes.node.isRequired
--- src/components/Home/Event/index.js
+++ src/components/Home/Event/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { format } from "date-fns";
+import { ScreenReaderContent } from "@components/Common";
 import { Wrapper, Time, Title, Community, Link } from "./UI";
 
 const Event = ({ data }) => (
@@ -9,9 +10,10 @@ const Event = ({ data }) => (
       {format(data.get("date").replace("T00:00:00.000Z", ""), "DD.MM")}
     </Time>
     <Title itemProp="name">{data.get("name")}</Title>
+    <ScreenReaderContent itemProp="location">{data.get("location")}</ScreenReaderContent>
     <Community itemProp="organizer">{data.get("community")}</Community>
     <Link href={data.get("url")} target="_blank" rel="noopener" itemProp="url">
-      Visite o site do evento
+      Visite o site do evento <ScreenReaderContent>{data.get("name")}</ScreenReaderContent>
     </Link>
   </Wrapper>
 );
