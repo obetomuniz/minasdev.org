a6d5c54d4f7eff45702770d48d49ac3f86f0dbe4
Beto Muniz <contato@betomuniz.com>
2020-06-18 04:03:37 -0300

    Add font-display: block

--- components/Common/App/ui.js
+++ components/Common/App/ui.js
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Montserrat';
     font-style: normal;
     font-weight: 300;
-    font-display: swap;
+    font-display: block;
     src: local('Montserrat Light'), local('Montserrat-Light'), url(https://fonts.gstatic.com/s/montserrat/v14/JTURjIg1_i6t8kCHKm45_cJD3gnD_vx3rCs.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
@@ -13,7 +13,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Montserrat';
     font-style: normal;
     font-weight: 400;
-    font-display: swap;
+    font-display: block;
     src: local('Montserrat Regular'), local('Montserrat-Regular'), url(https://fonts.gstatic.com/s/montserrat/v14/JTUSjIg1_i6t8kCHKm459WlhyyTh89Y.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
@@ -21,7 +21,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Montserrat';
     font-style: normal;
     font-weight: 600;
-    font-display: swap;
+    font-display: block;
     src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url(https://fonts.gstatic.com/s/montserrat/v14/JTURjIg1_i6t8kCHKm45_bZF3gnD_vx3rCs.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
@@ -29,7 +29,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Montserrat';
     font-style: normal;
     font-weight: 700;
-    font-display: swap;
+    font-display: block;
     src: local('Montserrat Bold'), local('Montserrat-Bold'), url(https://fonts.gstatic.com/s/montserrat/v14/JTURjIg1_i6t8kCHKm45_dJE3gnD_vx3rCs.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
@@ -37,7 +37,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Montserrat';
     font-style: normal;
     font-weight: 800;
-    font-display: swap;
+    font-display: block;
     src: local('Montserrat ExtraBold'), local('Montserrat-ExtraBold'), url(https://fonts.gstatic.com/s/montserrat/v14/JTURjIg1_i6t8kCHKm45_c5H3gnD_vx3rCs.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
@@ -45,7 +45,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Montserrat';
     font-style: normal;
     font-weight: 900;
-    font-display: swap;
+    font-display: block;
     src: local('Montserrat Black'), local('Montserrat-Black'), url(https://fonts.gstatic.com/s/montserrat/v14/JTURjIg1_i6t8kCHKm45_epG3gnD_vx3rCs.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
