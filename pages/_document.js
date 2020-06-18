72aba929f0dd5b65c8639e29dcac0dfd1a3b978e
Beto Muniz <contato@betomuniz.com>
2020-06-18 00:59:50 -0300

    Improve rendering blocks

--- pages/_document.js
+++ pages/_document.js
@@ -146,6 +146,7 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700;800;900&display=block"
             rel="stylesheet"
+            async
           />
 
           {/* Google Analytics */}
